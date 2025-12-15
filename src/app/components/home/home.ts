989dac3599674f9fa50ba93b96eb9f0e102432bc
Asritha Kuruguntla <asritha.kuruguntla@gmail.com>
2025-12-15 15:09:22 +0530

    form handling for searching flights added

--- src/app/components/home/home.ts
+++ src/app/components/home/home.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { searchReq } from '../../models/searchReq';
 import { Router } from '@angular/router';
-// import { FlightList } from '../flight-list/flight-list';
 @Component({
   selector: 'app-home',
-  imports: [FormsModule],
+  imports: [ReactiveFormsModule],
   standalone:true,
   templateUrl: './home.html',
   styleUrl: './home.css',
@@ -16,8 +15,17 @@ export class Home {
   origin: '',
   destination: ''
 };
+form=new FormGroup(
+  {
+origin: new FormControl('',[Validators.required,Validators.pattern('^[A-Za-z ]+$')]),
+destination:new FormControl('',[Validators.required,Validators.pattern('^[A-Za-z ]+$')])
+
+  }
+
+);
 onSubmit() {
-  console.log("clicked search button");
+  this.req.origin=this.form.value.origin!;
+  this.req.destination=this.form.value.destination!;
   this.router.navigate(['/flights'], {
     state:{
       searchData:{
