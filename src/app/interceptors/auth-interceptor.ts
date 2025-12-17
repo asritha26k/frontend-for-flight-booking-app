c2448478b375b15b6d2d496da70ff7d15caab843
Asritha Kuruguntla <asritha.kuruguntla@gmail.com>
2025-12-17 12:36:21 +0530

    assessingbugs in interceptor with comments

--- src/app/interceptors/auth-interceptor.ts
+++ src/app/interceptors/auth-interceptor.ts
@@ -5,10 +5,14 @@ import { catchError, throwError } from 'rxjs';
 
 export const authInterceptor: HttpInterceptorFn = (req, next) => {
   const router = inject(Router);
+  
+  console.log(' Interceptor hit:', req.url);
 
   return next(req).pipe(
     catchError(err => {
+       console.log('Interceptor error:', err.status);
       if (err.status === 401 && router.url !== '/signin') {
+        console.log('Redirecting to signin');
         router.navigate(['/signin']);
       }
       return throwError(() => err);
